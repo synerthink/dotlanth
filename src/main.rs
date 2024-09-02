07a702271c4884b2c266c21a88be40944bd5dfc3
Adem Onur Çelik <ademonurcelik@icloud.com>
2024-09-02 21:40:09 +0300

    feat: demonstration of execution engine & error handling

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod core;
 mod logging;
 
 use contracts::SimpleAdditionContract;
-use core::{
-    execution_engine::ExecutionEngine, protocol_manager::protocol_manager::ProtocolManager,
-};
+use core::
+    execution_engine::ExecutionEngine
+;
 use logging::setup_logging;
 
 use log::info;
@@ -14,21 +14,14 @@ fn main() {
     setup_logging();
     info!("Starting dotVM...");
 
-    // Test Execution Engine
-    let contract = SimpleAdditionContract { a: 5, b: 3 };
+    // Create a SimpleAdditionContract with the source code
+    let contract = SimpleAdditionContract::new();
+
+    // Execute the contract
     let result = ExecutionEngine::execute_contract(&contract);
 
     match result {
         Ok(res) => println!("Execution result: {:?}", res),
         Err(err) => println!("Execution error: {:?}", err),
     }
-
-    // Test Protocol Manager
-    let mut protocol_manager = ProtocolManager::new();
-    let protocol = protocol_manager.create_protocol(
-        "ExpenseApproval".to_string(),
-        "Protocol for approving expenses".to_string(),
-    );
-
-    println!("{:?}", protocol)
 }
