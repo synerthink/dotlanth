60d4c2f3656ea67a73574d112ee7e91823e6fa01
Adem Onur Çelik <ademonurcelik@icloud.com>
2025-01-09 04:10:10 +0300

    fmt: cargo fmt

--- crates/tools/benches/benchmarks.rs
+++ crates/tools/benches/benchmarks.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 fn benchmark_main_initialization(c: &mut Criterion) {
     c.bench_function("main_init", |b| {
