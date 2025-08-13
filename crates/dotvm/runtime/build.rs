a25f5eb813865d26fd4cc6091d8808cbc3cd2019
onurbbr <onurbuber@engineer.com>
2025-08-13 11:38:36 +0300

    feat: enable database service compilation in build script
    - Add database_service.proto to tonic build configuration
    - Include cluster_service.proto and common.proto in compilation
    - Remove commented TODO for database service enabling

--- crates/dotvm/runtime/build.rs
+++ crates/dotvm/runtime/build.rs
@@ -19,11 +19,16 @@ use std::{env, error::Error, path::PathBuf};
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    tonic_build::configure()
-        .file_descriptor_set_path(out_dir.join("runtime_descriptor.bin"))
-        .compile(&["proto/runtime.proto", "proto/vm_service.proto"], &["proto"])?;
-    // TODO: Enable when database service is ready
-    // tonic_build::compile_protos("proto/database_service.proto")?;
+    tonic_build::configure().file_descriptor_set_path(out_dir.join("runtime_descriptor.bin")).compile(
+        &[
+            "proto/runtime.proto",
+            "proto/vm_service.proto",
+            "proto/database_service.proto",
+            "proto/cluster_service.proto",
+            "proto/common.proto",
+        ],
+        &["proto"],
+    )?;
 
     Ok(())
 }
